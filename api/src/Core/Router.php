3eafbf572efb60743b7f2efb4ff1dab3797a93ed
Bryn Mailer <bryn@trustprovenance.com>
2020-08-24 09:52:06 +1000

    started api framework overhaul

---
+++ api/src/Core/Router.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace InvMan\Core;
+
+class Router {
+  public function map(
+    string $method,
+    string $path,
+    string $handler
+  ): void {
+    
+  }
+}
